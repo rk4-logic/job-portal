d507a5403392a78670201a554e74de003292aa09
rk4-logic <rhtprjpt54@gmail.com>
2026-04-28 16:59:11 +0530

    Photo uploading tool added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{ hostname: "we2plp04mq.ufs.sh", protocol: "https" }],
+  },
 };
 
 export default nextConfig;
